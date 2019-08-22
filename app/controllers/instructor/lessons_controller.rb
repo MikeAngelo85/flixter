f0f9af294ec6a72b66ff5e0f75374b2dc01ee893
Michaelangelo Ramos <ramos.michael85@gmai.com>
2019-08-22 08:27:50 +0000

    add permissions

---
+++ app/controllers/instructor/lessons_controller.rb
@@ -0,0 +1,24 @@
+class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
+  end
+
+  private
+
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle)
+  end
+end
