e4959b7d63b1e7e7a9942d10a588ff4d210200b5
Michaelangelo Ramos <ramos.michael85@gmai.com>
2019-09-08 11:00:42 +0000

    add video embedding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :coursese
+  has_many :courses
   # Include default devise moduls. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
