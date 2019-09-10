9b0234a12012dd82efd1a4fc1e9c748de5192225
Michaelangelo Ramos <ramos.michael85@gmai.com>
2019-09-10 09:10:43 +0000

    require user to be enroled to view

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_user, only: [:show]
+
   def show
   end
 
-  private
+   private
+
+  def require_authorized_for_current_user
+      if !current_user.enrolled_in?(current_lesson.section.course)
+          redirect_to course_path, alert: 'Enroll to access course'
+  end
+end
 
   helper_method :current_lesson
   def current_lesson
